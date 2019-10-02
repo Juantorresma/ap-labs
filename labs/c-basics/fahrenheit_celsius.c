3094c9b19956ed71e11567c1257e8673fe1cf215
Juantorresma <31540688+Juantorresma@users.noreply.github.com>
2019-10-02 18:28:19 -0500

    Update fahrenheit_celsius.c

--- labs/c-basics/fahrenheit_celsius.c
+++ labs/c-basics/fahrenheit_celsius.c
@@ -1,27 +1,32 @@
-#include <stdio.h>
+//A01227885
 
-#define   LOWER  0       /* lower limit of table */
-#define   UPPER  300     /* upper limit */
-#define   STEP   20      /* step size */
+#include <stdio.h>
 
 /* print Fahrenheit-Celsius table */
 
 int main(int argc, char **argv)
 {
+    int fahr;
+    int start;
+    int end;
+    int increment;
+    
     if (argc == 2){
-    printf("Fahrenheit: %3d, Celcius: %6.1f\n", atoi(argv[1]), (5.0/9.0)*(atoi(argv[1])-32));
-
-
-  } else if (argc == 4){
-    int fahr, lower, upper, stepnext
-    lower = atoi(argv[1]);
-    upper = atoi(argv[2]);
-    next = atoi(argv[3]);
-
-
-     for (fahr = lower; fahr <= upper; fahr = fahr + next)
-	printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+        fahr =  atoi(argv[1]);
+        printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
     }
-	
+    
+    
+     if (argc == 4){
+         start =  atoi(argv[1]);
+         end =  atoi(argv[2]);
+         increment =  atoi(argv[3]) ;
+         
+         
+        for (fahr = start; fahr <= end; fahr = fahr + increment)
+	    printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+     }
     return 0;
 }
+
+
