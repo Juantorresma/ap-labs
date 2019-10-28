72884c533260e428f285c1f4d14ea766bc798692
Juantorresma <31540688+Juantorresma@users.noreply.github.com>
2019-10-27 21:43:53 -0600

    Update mytop.c

--- challenges/second-partial/mytop/mytop.c
+++ challenges/second-partial/mytop/mytop.c
@@ -1,215 +1,236 @@
-// Fernando Martinez
-// A01630401
+//My program A01227885 mytop Second partial
 
 #include <stdio.h>
-#include <dirent.h>
-#include <string.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <sys/inotify.h>
+#include <dirent.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
 
-#define EVENT_SIZE (sizeof(struct inotify_event))
-#define EVENT_BUF_LEN (1024*(EVENT_SIZE + 16))
+//Here i define my dtruct which i will use in my program
 
-void clear();
+typedef struct node {
+	int pid;
+	int ppid;
+	char *name;
+	char *state;
+	int memory;
+	int threads;
+	int files;
+	struct node * next;
+} 
 
-void getProcesses();
+//The nodes i will be using, start my headd in null
+node_t;
+node_t * head = NULL;
 
-void checkChanges();
+//My push function
+void push(int pid, int ppid, char *name, char *state, int memory, int threads, int files) {
+	node_t * current = head;
+	node_t * parent = NULL;
+	int newHead = 1;
+    while (current != NULL && current->memory > memory) {
+		newHead = 0;
+		parent = current;
+        current = current->next;
+    }
+	node_t *tmp = current;
+    current = malloc(sizeof(node_t));
+	current->pid = pid;
+	current->ppid = ppid;
+	current->name = name;
+	current->state = state;
+	current->memory = memory;
+	current->threads = threads;
+	current->files = files;
+    current->next = tmp;
+	if(newHead){
+		head = current;
+	}else{
+		parent->next = current;
+	}
+}
 
-int main(){
-	clear();
-	getProcesses();
-	checkChanges();
-	// Place your magic here
-	//clear();
+//My clean funtion
+void clean(node_t * head){
+	node_t * current = head;
+	node_t * tmp;
+    while (current != NULL) {
+		tmp = current->next;
+		free(current);
+        current = tmp;
+    }
+}
+
+//My proc function
+int proc(){
+	head = NULL;
+	DIR *d;
+	struct dirent *dir;
+	d = opendir("/proc/");
+	if (d)
+	{
+		while ((dir = readdir(d)) != NULL)
+		{
+			if (isdigit((dir->d_name)[0])) {
+				char *path = (char *)malloc(sizeof(char)*(6+strlen(dir->d_name)+8));
+				strcat(path, "/proc/");
+				strcat(path, dir->d_name);
+				strcat(path, "/status");
+				FILE *fp = fopen(path, "r");
+				char * line = NULL;
+			    size_t len = 0;
+			    ssize_t read;
+				if (fp == NULL){
+					printf("Can't open file %s\n", path);
+					return -1;
+				}
+				int pid;
+				int ppid;
+				char *name;
+				char *state;
+				int memory;
+				int threads;
+				int files;
+				char *substr;
+				char *p;
+				while ((read = getline(&line, &len, fp)) != -1) {
+					if (line[read - 1] == '\n')
+				    {
+				        line[read - 1] = '\0';
+				        --read;
+				    }
+					// Pid
+					substr = "Pid:\t";
+					p = strstr(line, substr);
+					if(p == line) {
+						pid = atoi((p+5));
+					}
+					// Parent id
+					substr = "PPid:\t";
+					p = strstr(line, substr);
+					if(p == line) {
+						ppid = atoi((p+6));
+					}
+					// Name
+					substr = "Name:\t";
+					p = strstr(line, substr);
+					if(p == line) {
+						p = (p+6);
+						name = (char *)malloc(sizeof(char)*(strlen(p)+1));
+						strcpy(name, p);
+					}
+					// State
+					substr = "State:\t";
+					p = strstr(line, substr);
+					if(p == line) {
+						p = (p+10);
+						p[strlen(p) - 1] = '\0';
+						state = (char *)malloc(sizeof(char)*(strlen(p)));
+						strcpy(state, p);
+					}
+					// Memory
+					substr = "VmSize:\t";
+					p = strstr(line, substr);
+					if(p == line) {
+						p = (p+8);
+						p[strlen(p) - 3] = '\0';
+						memory = atoi(p)/1024;
+					}
+					// Threads
+					substr = "Threads:\t";
+					p = strstr(line, substr);
+					if(p == line) {
+						threads = atoi((p+9));
+					}
+
+				}
+				fclose(fp);
+				free(path);
+
+				char *fdpath = (char *)malloc(sizeof(char)*(6+strlen(dir->d_name)+5));
+				strcat(fdpath, "/proc/");
+				strcat(fdpath, dir->d_name);
+				strcat(fdpath, "/fd/");
+				DIR *procfd;
+				struct dirent *fd;
+				procfd = opendir(fdpath);
+				if (procfd)
+				{
+					files = -2;
+					while ((fd = readdir(procfd)) != NULL)
+					{
+						files++;
+					}
+				}
+				closedir(procfd);
+
+				push(pid, ppid, name, state, memory, threads, files);
+
+				free(fdpath);
+				if (line)
+		 		   free(line);
+			}
+		}
+		closedir(d);
+	}
 	return 0;
 }
 
-void checkChanges(){
-	int fd;
-        int wd;
-        int rd;
-        int p = 0;
-        struct inotify_event *event;
-        char buffer[EVENT_BUF_LEN];
-        fd = inotify_init();
-        if(fd<0)
-                printf("inotify error\n");
-        wd = inotify_add_watch(fd, "/proc", IN_CREATE | IN_DELETE);
-        while(1) {
-                rd = read(fd, buffer, EVENT_BUF_LEN);
-                if(read < 0)
-                        printf("event not read\n");
-                while(p < rd) {
-                        event = (struct inotify_event * ) &buffer[p];
-                        if(event->len) {
-                                clear();
-                                getProcesses();
-                                p += EVENT_SIZE + event->len;
-                        }
-                }
-        }
-        inotify_rm_watch(fd, wd);
-        close(fd);
-}
 
-void getProcesses() {
-	char procContent[200][15];
-	int lenAll = 0;
-	DIR *directory;
-	struct dirent *entry;
-	if((directory = opendir("/proc")) != NULL) {
-		while((entry = readdir(directory)) != NULL) {
-			strcpy(procContent[lenAll], entry->d_name);
-			lenAll++;
-		}
-		closedir(directory);
-	}
-	else
-		printf("Directory not open");
-
-
-	char allProcesses[1000][30];
-	int i;
-	int q;
-	int len2 = 0;
-	char data[10000];
-	for(i = 0; i <= lenAll; i++) {
-		if(atoi(procContent[i]) != 0) {
-			strcpy(allProcesses[len2], "/proc/");
-			strcat(allProcesses[len2], procContent[i]);
-			//strcat(allProcesses[len2], "/status");
-			//printf("%s\n", allProcesses[len2]);
-			len2++;
-		}
-	}
-	printf("| PID \t| Parent| Name                        | State    | Memory    | # Threads | Open Files |\n");
-	printf("|-------|-------|-----------------------------|----------|-----------|-----------|------------|\n");
-	for(q = 0; q < len2; q++) {
-		char fileName[30];
-		char files[30];
-		strcpy(fileName, allProcesses[q]);
-		strcat(fileName, "/status");
-		strcpy(files, allProcesses[q]);
-		strcat(files, "/fd/");
-		int fileDesc = open(fileName, O_RDONLY);
-		//printf("Name of the file:                  %s\n", allProcesses[q]);
-		if(fileDesc < 0) {
-			printf("File not found\n");
-			//printf("%s\n", allProcesses[i]);
-		}
-		else {
-			//printf("File read\n");
-			//printf("%s\n", allProcesses[i]);
-			//printf("\n");
-			int fileRead = read(fileDesc, data, 10000);
-			if(fileRead < 0) {
-				printf("File not read");
-			}
-			close(fileDesc);
-			data[fileRead] = '\0';
-			int u;
-			char pid[15];
-			char parent[15];
-			char name[30];
-			char status[20];
-			char memory[30];
-			char threads[15];
-			int oFiles = 0;
-			char *line;
-			char *toSearch;
-
-			toSearch = "Pid:\t";
-			line = strstr(data, toSearch);
-			if(line != NULL) {
-				u = 0;
-				line += 5;
-				while(line[u] != '\n') {
-					pid[u] = line[u];
-					u++;
-				}
-			}
-
-			toSearch = "PPid:\t";
-			line = strstr(data, toSearch);
-			if(line != NULL) {
-				u = 0;
-				line += 6;
-				while(line[u] != '\n') {
-					parent[u] = line[u];
-					u++;
-				}
-			}
-
-			toSearch = "Name:\t";
-			line = strstr(data, toSearch);
-			if(line != NULL) {
-				u = 0;
-				line += 6;
-				while(line[u] != '\n'){
-					name[u] = line[u];
-					u++;
-				}
-			}
-
-			toSearch = "State:\t";
-			line = strstr(data, toSearch);
-			if(line != NULL) {
-				u = 0;
-				line += 10;
-				while(line[u] != ')') {
-					status[u] = line[u];
-					u++;
-				}
-			}
-
-
-			toSearch = "VmSize:\t";
-			line = strstr(data, toSearch);
-			if(line != NULL) {
-				u = 0;
-				line += 10;
-				while(line[u] != '\n') {
-					memory[u] = line[u];
-					u++;
-				}
-			}
-
-			toSearch = "Threads:\t";
-			line = strstr(data, toSearch);
-			if(line != NULL) {
-				u = 0;
-				line += 9;
-				while(line[u] != '\n') {
-					threads[u] = line[u];
-					u++;
-				}
-			}
-
-			DIR *dirFiles;
-			if((dirFiles = opendir(files))!= NULL) {
-				while((readdir(dirFiles)) != NULL)
-					oFiles++;
-				closedir(dirFiles);
-			}
-
-
-			printf("| %s \t| %s \t| %27s | %8s | %9s | %9s | %10d |\n", pid, parent, name, status, memory, threads, oFiles);
-			int m;
-			for(m = 0; m < 50; m++) {
-				name[m] = '\0';
-				status[m] = '\0';
-				pid[m] = '\0';
-				parent[m] = '\0';
-				memory[m] = '\0';
-				threads[m] = '\0';
-                	}
-		}
-
-	}
-}
-
+//The clear function
 void clear() {
-	printf("\e[1;1H\e[2J");
+	printf("\e[1;1H\e[2J"); 
+}
+
+//Here i print the headers
+void print_header(){
+	printf("| %7s ", "PID");
+	printf("| %7s ", "Parent");
+	printf("| %36s ", "Name");
+	printf("| %8s ", "State");
+	printf("| %8s ", "Memory");
+	printf("| %9s ", "# Threads");
+	printf("| %10s |\n", "Open Files");
+	printf("|---------|---------|--------------------------------------|----------|----------|-----------|------------|\n");
+}
+
+//Here i ptint the nodes
+void print_node(node_t *current){
+	printf("| %7d ", current->pid);
+	printf("| %7d ", current->ppid);
+	printf("| %36s ", current->name);
+	printf("| %8s ", current->state);
+	printf("| %7dM ", current->memory);
+	printf("| %9d ", current->threads);
+	printf("| %10d |\n", current->files);
+}
+
+
+//My print function
+void print(){
+	print_header();
+  	node_t * current = head;
+	int i = 0;
+	while (current != NULL && i<20) {
+		i++;
+	    print_node(current);
+	    current = current->next;
+	}
+	clean(head);
+	sleep(2);
+	clear();
+}
+
+//An infinite loop to run my process proc
+int main(){
+	while(1){
+		int out = proc();
+		clear();
+		if(out!=0){
+			return -1;
+		}
+		print();
+	}
+	return 0;
 }
