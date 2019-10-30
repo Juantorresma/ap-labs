90515af1054cf3e62f4767f1637bb19abe454fb9
Juantorresma <31540688+Juantorresma@users.noreply.github.com>
2019-10-29 22:52:49 -0600

    Update logger.h

--- labs/file-dir-monitor/logger.h
+++ labs/file-dir-monitor/logger.h
@@ -1,7 +1,9 @@
-// Logger
+// Logger A01227885
 
 int initLogger(char *logType);
 int infof(const char *format, ...);
 int warnf(const char *format, ...);
 int errorf(const char *format, ...);
 int panicf(const char *format, ...);
+void textcolor(int attr, int fg, int bg);
+int printWithFormat(char *type, int color, const char *format, va_list arg);
