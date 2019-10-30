1dfc529b78c6df4e90143cdc7145e2535f33dfeb
Juantorresma <31540688+Juantorresma@users.noreply.github.com>
2019-10-29 23:10:09 -0600

    Update clock2.go

--- labs/clockwall/clock2.go
+++ labs/clockwall/clock2.go
@@ -1,26 +1,53 @@
 // Clock2 is a concurrent TCP server that periodically writes the time.
 package main
 
+//imports
 import (
 	"io"
 	"log"
 	"net"
 	"time"
+	"strconv"
+	"os"
 )
 
-func handleConn(c net.Conn) {
+//modified handle con for errors
+func handleConn(c net.Conn, tz string) {
 	defer c.Close()
+	loc, er := time.LoadLocation(tz)
+	if er != nil {
+		log.Fatal(er)
+		return // e.g., client disconnected
+	}
 	for {
-		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+		_, err := io.WriteString(c, tz+": "+time.Now().In(loc).Format("15:04:05\n"))
 		if err != nil {
+			log.Fatal(err)
 			return // e.g., client disconnected
 		}
 		time.Sleep(1 * time.Second)
 	}
 }
 
+//my new main with a flag which i will be using, and the name
 func main() {
-	listener, err := net.Listen("tcp", "localhost:9090")
+	flag:= false
+	var name string
+	for _, s := range os.Args {
+	    if s == "-port" {
+	        flag=true
+			continue
+	    }else if flag{
+			flag= false
+			i, err := strconv.Atoi(s)
+			if err != nil || i>65535 || i<0{
+				log.Fatal(err)
+			}
+			name = "localhost:"+strconv.Itoa(i)
+		}
+	}
+	tz := os.Getenv("TZ")
+	listener, err := net.Listen("tcp", name)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -30,6 +57,7 @@ func main() {
 			log.Print(err) // e.g., connection aborted
 			continue
 		}
-		go handleConn(conn) // handle connections concurrently
+
+		go handleConn(conn, tz) // handle connections concurrently
 	}
 }
