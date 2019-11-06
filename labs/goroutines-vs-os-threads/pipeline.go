b50600d38150e9422e29cbe2cd6904342ce81807
Juantorresma <31540688+Juantorresma@users.noreply.github.com>
2019-11-06 10:03:13 -0600

    Update pipeline.go

--- labs/goroutines-vs-os-threads/pipeline.go
+++ labs/goroutines-vs-os-threads/pipeline.go
@@ -10,20 +10,20 @@ import(
 const layout = "15:04:05.000000"
 
 func main() {
-	nPipes := 1000 
+	numPipes := 1000 
 	var channels = make([]chan string, 0)
   
-	for i := 0; i < nPipes; i++ {
+	for i := 0; i < numPipes; i++ {
 		channels = append(channels, make(chan string))
 	}
   
 	go firstPipe(channels[0])
   
-	for i := 1; i < nPipes; i++ {
+	for i := 1; i < numPipes; i++ {
 		go middlePipe(channels[i-1], channels[i])
 	}
   
-	startTime, _ := time.Parse(layout, <-channels[nPipes-1])
+	startTime, _ := time.Parse(layout, <-channels[numPipes-1])
 	diff := time.Since(startTime)
 	fmt.Println("Time it took for our message to pass through all channels and goroutines was:", diff)
   
