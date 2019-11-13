a4b529ddbb9e367920188a620230acccc2609e33
Juantorresma <31540688+Juantorresma@users.noreply.github.com>
2019-11-12 19:03:54 -0600

    Update crawl3.go

--- labs/web-crawler/crawl3.go
+++ labs/web-crawler/crawl3.go
@@ -1,34 +1,23 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-
-// See page 241.
-
-// Crawl2 crawls web links starting with the command-line arguments.
-//
-// This version uses a buffered channel as a counting semaphore
-// to limit the number of concurrent calls to links.Extract.
-//
-// Crawl3 adds support for depth limiting.
-//
+//A01227885 modified crawler
 package main
 
 import (
 	"fmt"
 	"log"
-	"os"
-
-	"gopl.io/ch5/links"
+	"flag"
+	"github.com/adonovan/gopl.io/ch5/links"
 )
 
-//!+sema
-// tokens is a counting semaphore used to
-// enforce a limit of 20 concurrent requests.
+//new function
 var tokens = make(chan struct{}, 20)
 
 func crawl(url string) []string {
 	fmt.Println(url)
 	tokens <- struct{}{} // acquire a token
 	list, err := links.Extract(url)
+	for _, elem := range list {
+		fmt.Println(elem)
+	}
 	<-tokens // release the token
 
 	if err != nil {
@@ -37,28 +26,29 @@ func crawl(url string) []string {
 	return list
 }
 
-//!-sema
-
-//!+
+//main
 func main() {
 	worklist := make(chan []string)
-	var n int // number of pending sends to worklist
-
-	// Start with the command-line arguments.
+	var n int 
+	var depth int;
+	var depthcounter int = 0;
 	n++
-	go func() { worklist <- os.Args[1:] }()
-
-	// Crawl the web concurrently.
+	flag.IntVar(&depth, "depth", 3, "User defined depth")
+	flag.Parse()
+	go func () { worklist <- flag.Args() }()
 	seen := make(map[string]bool)
 	for ; n > 0; n-- {
 		list := <-worklist
-		for _, link := range list {
-			if !seen[link] {
-				seen[link] = true
-				n++
-				go func(link string) {
-					worklist <- crawl(link)
-				}(link)
+		if depthcounter < depth {
+			for _, link := range list {
+				if !seen[link] {
+					seen[link] = true
+					depthcounter++
+					n++
+					go func(link string) {
+						worklist <- crawl(link)
+					}(link)
+				}
 			}
 		}
 	}
