7d0d2477c0a6fdc10a6f2a27fb4b8a699795649b
Juantorresma <31540688+Juantorresma@users.noreply.github.com>
2019-12-03 18:22:56 -0600

    Update pi_loop.c

--- labs/openmp/pi_loop.c
+++ labs/openmp/pi_loop.c
@@ -1,4 +1,27 @@
+#include <stdio.h>
+#include <omp.h>
+#include "logger.h"
 
-int main() {
-    return 0;
+static long num_steps = 1000000;
+#define NUM_THREADS 2
+double step;
+int main ()
+{
+    double x, pi, sum = 0.0;
+    double start_time, run_time;
+
+    step = 1.0/(double) num_steps;
+
+    start_time = omp_get_wtime();
+    omp_set_num_threads(NUM_THREADS);
+	#pragma omp parallel for private(x)reduction(+:sum)
+
+    for (int i=1;i<= num_steps; i++){
+	x = (i-0.5)*step;
+	sum = sum + 4.0/(1.0+x*x);
+    }
+
+    pi = step * sum;
+    run_time = omp_get_wtime() - start_time;
+    infof("\n pi with %d steps is %f in %f seconds ",num_steps,pi,run_time);
 }
